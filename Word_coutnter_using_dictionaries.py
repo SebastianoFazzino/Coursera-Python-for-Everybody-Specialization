ad2af3e341a2e867cb852b96e81a10d1147491df
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-08-26 16:25:19 +1200

    Add files via upload

---
+++ Word_coutnter_using_dictionaries.py
@@ -0,0 +1,25 @@
+#how to find the most common word in a text:
+
+
+file = open('Assignment.txt')
+counts = dict()
+
+for line in file:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+bigcount = None
+bigword = None
+
+for word, count in counts.items():
+   if bigcount is None or count > bigcount:
+      bigword = word
+      bigcount = count
+        
+       
+print(bigword,bigcount)
+
+
+
+
